fca88cbcf42be30b6832b8f732be70e2e5b44708
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-14 11:31:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,27 @@
+import os
 import streamlit as st
-import pandas as pd
-import numpy as np
-import requests
 
-# Create a page dropdown 
-page = st.sidebar.selectbox("Choose your page", ["Page 1", "Page 2", "Page 3"]) 
+# Custom imports 
+from multipage import MultiPage
+from pages import games_analysis, network_analysis, player_performance_prediction, clustering # import your pages here
 
-if page == "Page 1":
-    # Display details of page 1
+# Create an instance of the app 
+app = MultiPage()
 
-    st.title('Uber pickups in NYC')
+# Title of the main page
+#display = Image.open('Logo.png')
+#display = np.array(display)
+# st.image(display, width = 400)
+# st.title("Data Storyteller Application")
+#col1, col2 = st.beta_columns(2)
+#col1.image(display, width = 400)
+#col2.title("Data Storyteller Application")
 
-    DATE_COLUMN = 'date/time'
-    DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+# Add all your application here
+app.add_page("Games Analysis", games_analysis.app)
+app.add_page("Network Analysis", network_analysis.app)
+app.add_page("Player Performance Prediction", player_performance_prediction.app)
+app.add_page("Clustering", clustering.app)
 
-    @st.cache
-    def load_data(nrows):
-        data = pd.read_csv(DATA_URL, nrows=nrows)
-        lowercase = lambda x: str(x).lower()
-        data.rename(lowercase, axis='columns', inplace=True)
-        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-        return data
-
-    data_load_state = st.text('Loading data...')
-    data = load_data(10000)
-    data_load_state.text("Done! (using st.cache)")
-
-    if st.checkbox('Show raw data'):
-        st.subheader('Raw data')
-        st.write(data)
-
-    st.subheader('Number of pickups by hour')
-    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-    st.bar_chart(hist_values)
-
-    # Some number in the range 0-23
-    hour_to_filter = st.slider('hour', 0, 23, 17)
-    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-    st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-    st.map(filtered_data)
-
-if page == "Page 2":
-    st.title('PAGE 2 TITLE HEADER')
-    st.markdown('TEST TEST1 TEST2 TEST3')
+# The main app
+app.run()
