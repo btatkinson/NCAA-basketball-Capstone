3a916c2c2c128ddbb61ae668cb6a94156c0778bd
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-14 14:26:30 -0400

    Update games_analysis.py

--- pages/games_analysis.py
+++ pages/games_analysis.py
@@ -5,7 +5,7 @@ import requests
 
 def app():
     df = pd.read_csv('src/test_games.csv')
-    st.dataframe(df['label'].unqiue())
-    #option = st.selectbox(
-     #'Please choose a game...',
-     #df['label'].unqiue())
+    
+    option = st.selectbox(
+     'Please choose a game...',
+     df['label'].unique())
