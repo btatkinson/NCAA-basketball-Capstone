b5e9ee5b176a400c15ec65c4a9082b05a6679495
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-14 22:36:13 -0400

    Update games_analysis.py

--- pages/games_analysis.py
+++ pages/games_analysis.py
@@ -143,3 +143,4 @@ def app():
     
     final_chart = alt.vconcat((band+home_line+away_line),(h_bar&score_diff_line&a_bar)).configure_axis(gridOpacity=.5).configure_view(strokeWidth=0)
     st.altair_chart(final_chart)
+    st.dataframe(a_change)
