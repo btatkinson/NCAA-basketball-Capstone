d0498917bf3d071f23ee6f77a530c894d1b7a59a
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-14 22:33:54 -0400

    Update games_analysis.py

--- pages/games_analysis.py
+++ pages/games_analysis.py
@@ -139,7 +139,7 @@ def app():
         color=colorbar,
         opacity=opacity_cond,
         tooltip=['lineup','point_diff_stint','point_diff','time_played']
-    ).properties(height=50,width=650).add_selection(sel, brush)
+    ).properties(height=50,width=650).add_selection(sel)
     
     final_chart = alt.vconcat((band+home_line+away_line),(h_bar&score_diff_line&a_bar)).configure_axis(gridOpacity=.5).configure_view(strokeWidth=0)
     st.dataframe(a_change)
