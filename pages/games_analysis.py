9293ba6684d600b3a7fbfaf70acb09c086358513
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-18 14:39:04 -0400

    Update games_analysis.py

--- pages/games_analysis.py
+++ pages/games_analysis.py
@@ -17,7 +17,7 @@ def team_pbp_df(school, nickname, year):
 
   # filter for top team market and name and season in both home and away columns
   # grab the unique ids for those games
-  team_games = df[((df['play.on_court.home.market'] == school) & (df['play.on_court.home.name'] == nickname) & (df['year'] == year)) | 
+  team_games = df[((df['play.on_court.home.market'] == school) & (df['play.on_court.home.name'] == nickname) & (df['year'] == year)) | \
               ((df['play.on_court.away.market'] == school) & (df['play.on_court.away.name'] == nickname) & (df['year'] == year))]['meta.id'].unique()
 
   # filter the original dataframe for those game_ids
