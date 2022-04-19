1db85a8e653568a3f26fa0f334e4ff00e0919b7b
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-19 11:23:10 -0400

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
-st.title('Landing Page Title Goes Here')
-st.markdown('This is where I envision the required blog post will go.')
+def app():
+  
+  st.title('Landing Page Title Goes Here')
+  st.markdown('This is where I envision the required blog post will go.')
