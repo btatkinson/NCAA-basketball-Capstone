4a5e7cece34e6b39c8f869d1c101a3dc9e4ce36a
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-18 21:31:19 -0400

    Update games_analysis.py

--- pages/games_analysis.py
+++ pages/games_analysis.py
@@ -222,7 +222,8 @@ def app():
 
     home_line = alt.Chart(score_id).mark_line(strokeWidth=4,color=alt.HexColor(h_color)).encode(
         x='time',
-        y='home'
+        y='home',
+        color = 'home_market:N'
     ).properties(width=650)
 
     away_line = alt.Chart(score_id).mark_line(strokeWidth=4,color=alt.HexColor(a_color)).encode(
