d62fe5a4909cf67724a42eaf499097924fb339cb
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-19 11:22:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,7 @@ app = MultiPage()
 #col2.title("Data Storyteller Application")
 
 # Add all your application here
+app.add_page("About", about.app)
 app.add_page("Play-by-Play + Substitution", games_analysis.app)
 app.add_page("Network Analysis", network_analysis.app)
 app.add_page("Player Performance Prediction", player_performance_prediction.app)
