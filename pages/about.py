22ea4454809b756379a2874884becd34b5be3653
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-19 13:51:50 -0400

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -4,3 +4,19 @@ def app():
   
   st.title('Landing Page Title Goes Here')
   st.markdown('This is where I envision the required blog post will go.')
+  
+  st.header('Play-by-Play + Substitution')
+  st.markdown('Insert brief description about page')
+  
+  st.header('Network analysis')
+  st.markdown('Insert brief description about page')
+  
+  st.header('Player Performance Prediction')
+  st.markdown('Insert brief description about page')
+  
+  st.header('Clustering')
+  st.markdown('Insert brief description about page')
+  
+  st.header('References?')
+  st.markdown('List references here')
+            
