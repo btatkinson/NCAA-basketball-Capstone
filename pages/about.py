34a48fb2e22860f5ab78e678673ca05ea354501b
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-20 11:47:58 -0400

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -2,21 +2,30 @@ import streamlit as st
 
 def app():
   
+  ########## LANDING PAGE CODE ##########
   st.title('Landing Page Title Goes Here')
   st.markdown('This is where I envision the required blog post will go.')
   
+  ########## PLAY-BY-PLAY + SUBSTITUTION SECTION CODE ##########
   st.header('Play-by-Play + Substitution')
   st.markdown('Insert brief description about page')
+  st.subheader('Preliminary Data Sample')
+  df = pd.read_csv('src/test_games.csv', nrows = 1000)
+  st.dataframe(df)
   
-  st.header('Network analysis')
+  ########## NETWORK ANALYSIS SECTION CODE ##########
+  st.header('Network Analysis')
   st.markdown('Insert brief description about page')
   
+  ########## PLAYER PERFORMANCE PREDICTION SECTION CODE ##########
   st.header('Player Performance Prediction')
   st.markdown('Insert brief description about page')
   
+  ########## CLUSTERING SECTION CODE ##########
   st.header('Clustering')
   st.markdown('Insert brief description about page')
   
+  ########## REFERENCES SECTION CODE ##########
   st.header('References?')
   st.markdown('List references here')
             
