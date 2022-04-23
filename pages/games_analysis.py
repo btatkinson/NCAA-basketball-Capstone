dfaa984e3cac1e9dc8400ac1fc5f3d3061d0a16b
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-23 16:05:59 -0400

    Update games_analysis.py

--- pages/games_analysis.py
+++ pages/games_analysis.py
@@ -14,43 +14,15 @@ def team_pbp_df(school, year):
         a value of 2020 (????? MAYBE ????? NEEDS FURTHER EVALUATION)
   """
 
-  df['scheduled_date'] = pd.to_datetime(df['meta.scheduled'])
-
-  bins_dt = pd.date_range('2017-07-01', freq='365D', periods=6)
-  bins_str = bins_dt.astype(str).values
-
-  df['season'] = pd.cut(df['scheduled_date'].astype(np.int64)//10**9,
-                   bins=bins_dt.astype(np.int64)//10**9,
-                   labels=['2018','2019','2020','2021','2022'])
-
-  df['season'] = df['season'].astype(str)
-  
-  # find how many games we have for each team-season in the db
-
-  # cut the number of cols, drop dupes, get counts, and reset index
-  home = df[['meta.id','play.on_court.home.market','play.on_court.home.name','season']].drop_duplicates()[['play.on_court.home.market','play.on_court.home.name','season']].value_counts().reset_index()
-  away = df[['meta.id','play.on_court.away.market','play.on_court.away.name','season']].drop_duplicates()[['play.on_court.away.market','play.on_court.away.name','season']].value_counts().reset_index()
-
-  # change names of columns
-  home.columns = ['market','name','season','count']
-  away.columns = ['market','name','season','count']
-
-  # stack home and away datasets
-  ha = pd.concat([home,away])
-
-  # sum up number of home and away games for each team-season
-  game_counts = ha.groupby(['market','name','season'],as_index=False)['count'].sum().sort_values(by='count',ascending=False).reset_index(drop=True)
-  
+  # filter for top team market and name and season in both home and away columns
+  # grab the unique ids for those games
   team_games = df[(df['play.on_court.home.market'] == school) | (df['play.on_court.away.market'] == school)]['meta.id'].unique()
 
   # filter the original dataframe for those game_ids
   team = df[(df['meta.id'].isin(team_games))]
 
-  # check to make sure the math is correct
-  assert len(team['meta.id'].unique()) == game_counts.head(1)['count'].iloc[0]
-  
   ### STACK DATAFRAMES ON TOP OF EACH OTHER
-  # so that we don't have to check home/away for auburn data
+  # so that we don't have to check home/away
 
   # only grab the columns without home/away
   aw = [i for i in team.columns if 'home' not in i]
@@ -67,8 +39,8 @@ def team_pbp_df(school, year):
   # stack home/away dfs on top of each other
   combo = pd.concat([away_df,home_df])
 
-  combo = combo[combo['season'] == season]
-  
+  combo = combo[combo['year'] == year]
+
   return combo
 
 def player_oncourt_season(pbp_df, school, playername):
@@ -86,21 +58,17 @@ def player_oncourt_season(pbp_df, school, playername):
 
     # create a list of columns to keep in the resulting df
     cols_to_keep = ['meta.id','meta.scheduled','play.id','play.clock','play.description','play.team_points','play.event_type','play.on_court.team.market',
-                  'play.on_court.team.name','type', 'made', 'shot_type', 'three_point_shot','player.full_name', 'player.jersey_number', 'rebound_type', 
-                  'points','free_throw_type', 'shot_type_desc', 'play.clock.seconds','play.period', 'play.clock.seconds_game', 'play.clock.time_elapsed',
-                  'play.on_court.team.player1.full_name','play.on_court.team.player2.full_name','play.on_court.team.player3.full_name',
-                  'play.on_court.team.player4.full_name','play.on_court.team.player5.full_name','meta.team.color', 'label']
-    
+                    'play.on_court.team.name','type', 'made', 'shot_type', 'three_point_shot','player.full_name', 'player.jersey_number', 'rebound_type', 
+                    'points','free_throw_type', 'shot_type_desc', 'play.clock.seconds','play.period', 'play.clock.seconds_game', 'play.clock.time_elapsed',
+                    'play.on_court.team.player1.full_name','play.on_court.team.player2.full_name','play.on_court.team.player3.full_name',
+                    'play.on_court.team.player4.full_name','play.on_court.team.player5.full_name', 'label']
     #filter to include columns
     filt = pbp_df[cols_to_keep]
-    
     # filter to only include rows that are null or the exact school
     # sometimes pbp rows are null because they are things like "end of first half"
     filt = filt[(filt['play.on_court.team.market'].isnull()) | (filt['play.on_court.team.market'] == school)]
-    
     # Sort vlaues by game and seconds remaining in game, fillna with previous values
     filt = filt.sort_values(by=['meta.scheduled','play.clock.seconds_game'],ascending=False).fillna(method='ffill')
-    
     # create a column with comma separated player names
     filt['on_court_players'] = filt[['play.on_court.team.player1.full_name','play.on_court.team.player2.full_name','play.on_court.team.player3.full_name',
         'play.on_court.team.player4.full_name','play.on_court.team.player5.full_name']].agg(', '.join, axis = 1)
@@ -108,57 +76,40 @@ def player_oncourt_season(pbp_df, school, playername):
     # new df with a new column that states whether or not target player is on the court
     gs = filt.copy()
     gs['player_on'] = gs['on_court_players'].str.contains(playername)
+
+
+    ### PROBLEM AREA
+    # I think something is going wrong with the creation of the "to" column
+    # Shifting the column with the groupby is not working to
+    # There are times that the to column in the resulting data is a number way smaller than the from column
     
-    # create a dataframe that has all seconds from 0-2399
-    # eventually we will create a bar chart that has a bar for each game second whose bars go to 1 when the player is on court
-    all_times = pd.DataFrame(list(range(0,2400)),columns=['play.clock.seconds_game']).sort_values(by='play.clock.seconds_game',ascending=False)
+    # truncate columns
+    line_df = gs[['meta.id','meta.scheduled','play.clock.seconds_game','player_on']]
+    # create to column by groupby and shift
+    line_df['to'] = line_df.groupby(['meta.id','meta.scheduled'],as_index=False)['play.clock.seconds_game'].shift(-1).fillna(0).astype(int)
+    # rename columns
+    line_df = line_df.rename(columns={'play.clock.seconds_game':'from','meta.id':'id'})
+    # change time remaing columns to count up towards 2400 (needed for viz)
+    line_df['from'] = abs(2400 - line_df['from'])
+    line_df['to'] = abs(2400 - line_df['to'])
+    # drop duplicates in to/from columns
+    line_df = line_df.drop_duplicates(subset=['from','to'])
+    # find the rows in which the player's status changes from on-court to off-court
+    line_df['change'] = line_df['player_on'].astype(int) == line_df['player_on'].astype(int).shift(1)
+
+    # filter to only include rows where on/off-court status is different
+    change = line_df[line_df['change'] == False]
+    change['to'] = change['from'].shift(-1).fillna(0)
+
+
+    trunc_line_df = change.copy()
+    # only include rows where player is on-court
+    trunc_line_df = trunc_line_df[(trunc_line_df['player_on'] == 1)]
     
-    # drop dupes and rename columns
-    gs_dedupe = gs.drop_duplicates(subset=['meta.id','play.clock.seconds_game']).rename(columns={'play.clock.seconds_game':'time',
-                                                                                             'meta.id':'meta_id',
-                                                                                             'meta.scheduled':'meta_scheduled'})
+    trunc_line_df = trunc_line_df.merge(df[['id','label']],left_on='id',right_on='id')
+    trunc_line_df['to'] = trunc_line_df['to'].replace(0,2400)
 
-    # merge with the all_times dataframe to fill in time gaps
-    gs_merged = (gs_dedupe.groupby(['meta_id','meta_scheduled']).apply(lambda group: all_times.merge(group[['time','player_on']],
-                                                                                                 left_on=['play.clock.seconds_game'],
-                                                                                                 right_on=['time'],how='left').fillna(method='bfill')))
-
-    # reset index and rename time col
-    gs_merged = gs_merged.reset_index().drop(columns=['level_2','time']).rename(columns={'play.clock.seconds_game':'time'})
-
-    # change time col from bool to int
-    gs_merged['player_on'] = gs_merged['player_on'].astype(int)
-    
-    # format mp column for each game
-    gs_merged['mp_decimal'] = gs_merged.groupby('meta_id')['player_on'].transform('sum')
-
-    from datetime import timedelta
-
-    def get_time_mm_ss(sec):
-      # create timedelta and convert it into string
-      td_str = str(timedelta(seconds=sec))
-
-      # split string into individual component
-      x = td_str.split(':')
-      return f"{x[-2]}:{x[-1]}"
-
-    gs_merged['mp'] = gs_merged['mp_decimal'].apply(lambda x: get_time_mm_ss(x))
-
-    # add for labeling purposes
-    gs_merged['mp'] = gs_merged['mp'] + ' minutes'
-    
-    # drop dupes from time for each game
-    gs_merged = gs_merged.drop_duplicates(['meta_id','time'])
-    
-    alt.data_transformers.disable_max_rows()
-    
-    gs_merged['half_time'] = 1200
-    
-    cols_to_keep_viz = ['time','player_on','label','half_time','meta_scheduled','mp']
-
-    gs_viz = gs_merged[cols_to_keep_viz]
-
-    return gs_viz
+    return trunc_line_df
 
 def app():
   
@@ -322,33 +273,11 @@ def app():
    
     individual_player_times = player_oncourt_season(team, option_team, option_player)
       
-    ### VISUALIZE
-
-    # this isn't behaving the way i want, but is used to label halftime
-    axis_labels = (
-        "datum.time == 0 ? 'Half' : datum.time == 1200 ? 'Half' : 'Half'"
-    )
-
-    bars = alt.Chart().mark_bar(width=1).encode(
-        x=alt.X('time',axis=alt.Axis(values=[0,1200,2400],labelExpr=axis_labels),scale=alt.Scale(domainMax=2400,domainMin=0,clamp=True,padding=0)),
-        y=alt.Y('player_on',axis=None)
-    ).properties(width=500,height=20)
-
-    text = alt.Chart().mark_text(align='right',baseline='middle',dx=-260).encode(
-        text='label'
-    )
-
-    text2 = alt.Chart().mark_text(align='left',baseline='middle',dx=260).encode(
-        text='mp'
-    )
-
-    ticks = alt.Chart().mark_rule(color='white',strokeDash=[1,1],size=2).encode(
-        x='half_time'
-    )
-
-    individual_player_chart = alt.layer(bars,text,text2,ticks, data=gs_viz).facet(
-                                        spacing=4,row=alt.Row('label',sort=alt.EncodingSortField(field='meta_scheduled'), header=alt.Header(title=None,labels=False))
-                              ).configure_axis(grid=False).configure_view(strokeWidth=1)
+    individual_player_chart = alt.Chart(individual_player_times).mark_bar().encode(
+                              x = alt.X('from', axis = alt.Axis(title = 'Seconds into Game')),
+                              x2 = 'to',
+                              y = alt.Y('label',sort=alt.EncodingSortField(field='meta.scheduled'), axis = alt.Axis(title = 'Games'))
+                              ).properties(width = 650)
       
     st.altair_chart(individual_player_chart)
     st.caption('''For the selected team, you can display the playing time of each player for all games in the game dropdown menu with disconnected bar
