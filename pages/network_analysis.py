775013706c2ce1fb4f7371d3cdacaf30a4a665c2
Blake Atkinson <blakeatkinson@gmail.com>
2022-04-23 22:48:26 -0400

    changing weird bug

--- pages/network_analysis.py
+++ pages/network_analysis.py
@@ -322,44 +322,44 @@ def app():
 
     # st.subheader('Map of all pickups at %s:00' % hour_to_filter)
     # st.map(filtered_data)
-import streamlit as st
-import pandas as pd
+# import streamlit as st
+# import pandas as pd
 
-def app():
+# def app():
   
-  ########## LANDING PAGE CODE ##########
-  st.title('Landing Page Title Goes Here')
-  st.markdown('This is where I envision the required blog post will go (perhaps extended throughout the subheader sections).')
+#   ########## LANDING PAGE CODE ##########
+#   st.title('Landing Page Title Goes Here')
+#   st.markdown('This is where I envision the required blog post will go (perhaps extended throughout the subheader sections).')
   
-  ########## PLAY-BY-PLAY + SUBSTITUTION SECTION CODE ##########
-  st.header('Play-by-Play + Substitution')
+#   ########## PLAY-BY-PLAY + SUBSTITUTION SECTION CODE ##########
+#   st.header('Play-by-Play + Substitution')
   
-  st.markdown('Insert brief description about page')
+#   st.markdown('Insert brief description about page')
   
-  st.subheader('Preliminary Data Sample')
-  df = pd.read_csv('src/test_games.csv', nrows = 1000)
-  df.drop(columns = ['Unnamed: 0'], inplace = True)
-  st.dataframe(df)
+#   st.subheader('Preliminary Data Sample')
+#   df = pd.read_csv('src/test_games.csv', nrows = 1000)
+#   df.drop(columns = ['Unnamed: 0'], inplace = True)
+#   st.dataframe(df)
   
-  ########## NETWORK ANALYSIS SECTION CODE ##########
-  st.header('Network Analysis')
-  st.markdown('Insert brief description about page')
+#   ########## NETWORK ANALYSIS SECTION CODE ##########
+#   st.header('Network Analysis')
+#   st.markdown('Insert brief description about page')
   
-  ########## PLAYER PERFORMANCE PREDICTION SECTION CODE ##########
-  st.header('Player Performance Prediction')
-  st.markdown('Insert brief description about page')
+#   ########## PLAYER PERFORMANCE PREDICTION SECTION CODE ##########
+#   st.header('Player Performance Prediction')
+#   st.markdown('Insert brief description about page')
   
-  ########## CLUSTERING SECTION CODE ##########
-  st.header('Clustering')
-  st.markdown('Insert brief description about page')
+#   ########## CLUSTERING SECTION CODE ##########
+#   st.header('Clustering')
+#   st.markdown('Insert brief description about page')
   
-  ########## STATEMENT OF WORK SECTION CODE ##########
-  st.header('Statement of Work')
-  st.markdown('Blake statement of work.')
-  st.markdown('Max statement of work.')
-  st.markdown('Zachary statement of work.')
-  st.markdown('Matthew statement of work.')
+#   ########## STATEMENT OF WORK SECTION CODE ##########
+#   st.header('Statement of Work')
+#   st.markdown('Blake statement of work.')
+#   st.markdown('Max statement of work.')
+#   st.markdown('Zachary statement of work.')
+#   st.markdown('Matthew statement of work.')
   
-  ########## REFERENCES SECTION CODE ##########
-  st.header('References?')
-  st.markdown('List references here')
+#   ########## REFERENCES SECTION CODE ##########
+#   st.header('References?')
+#   st.markdown('List references here')
