62f195b831e2d89fa5957307019d2adf06aba436
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-24 14:11:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,26 +3,16 @@ import streamlit as st
 
 # Custom imports 
 from multipage import MultiPage
-from pages import about, games_analysis, network_analysis, player_performance_prediction, clustering # import your pages here
+from pages import about, games_analysis, network_analysis, player_performance_prediction# import your pages here
 
 # Create an instance of the app 
 app = MultiPage()
-
-# Title of the main page
-#display = Image.open('Logo.png')
-#display = np.array(display)
-# st.image(display, width = 400)
-# st.title("Data Storyteller Application")
-#col1, col2 = st.beta_columns(2)
-#col1.image(display, width = 400)
-#col2.title("Data Storyteller Application")
 
 # Add all your application here
 app.add_page("About", about.app)
 app.add_page("Play-by-Play + Substitution", games_analysis.app)
 app.add_page("Network Analysis", network_analysis.app)
 app.add_page("Player Performance Prediction", player_performance_prediction.app)
-app.add_page("Clustering", clustering.app)
 
 # The main app
 app.run()
