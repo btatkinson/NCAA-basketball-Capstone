c8ab4fa3ffb8ed4b98beb75103aef09ba2687d70
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-23 20:28:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # Custom imports 
 from multipage import MultiPage
-from pages import about, network_analysis, player_performance_prediction, clustering # import your pages here
+from pages import about, games_analysis network_analysis, player_performance_prediction, clustering # import your pages here
 
 # Create an instance of the app 
 app = MultiPage()
@@ -19,7 +19,7 @@ app = MultiPage()
 
 # Add all your application here
 app.add_page("About", about.app)
-#app.add_page("Play-by-Play + Substitution", games_analysis.app)
+app.add_page("Play-by-Play + Substitution", games_analysis.app)
 app.add_page("Network Analysis", network_analysis.app)
 app.add_page("Player Performance Prediction", player_performance_prediction.app)
 app.add_page("Clustering", clustering.app)
