25f04b6ed5ca03167c839c29403ad2d542477175
mfzimolzak <mfzimolzak@users.noreply.github.com>
2022-04-24 14:14:55 -0400

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -4,30 +4,9 @@ import pandas as pd
 def app():
   
   ########## LANDING PAGE CODE ##########
-  st.title('Landing Page Title Goes Here')
-  st.markdown('This is where I envision the required blog post will go (perhaps extended throughout the subheader sections).')
-  
-  ########## PLAY-BY-PLAY + SUBSTITUTION SECTION CODE ##########
-  st.header('Play-by-Play + Substitution')
-  
-  st.markdown('Insert brief description about page')
-  
-  st.subheader('Preliminary Data Sample')
-  df = pd.read_csv('src/test_games.csv', nrows = 1000)
-  df.drop(columns = ['Unnamed: 0'], inplace = True)
-  st.dataframe(df)
-  
-  ########## NETWORK ANALYSIS SECTION CODE ##########
-  st.header('Network Analysis')
-  st.markdown('Insert brief description about page')
-  
-  ########## PLAYER PERFORMANCE PREDICTION SECTION CODE ##########
-  st.header('Player Performance Prediction')
-  st.markdown('Insert brief description about page')
-  
-  ########## CLUSTERING SECTION CODE ##########
-  st.header('Clustering')
-  st.markdown('Insert brief description about page')
+  st.title('SIADS 697/698 Capstone Project')
+  st.header('About')
+  st.markdown('Words go here.')
   
   ########## STATEMENT OF WORK SECTION CODE ##########
   st.header('Statement of Work')
